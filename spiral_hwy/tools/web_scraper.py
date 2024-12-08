5a05d0e74bb30df0b34544c1aa83bba179ba21bb
Matthew Gunther <spiralhwy@gmail.com>
2024-12-07 22:01:42 -0800

    fix

--- spiral_hwy/tools/web_scraper.py
+++ spiral_hwy/tools/web_scraper.py
@@ -25,9 +25,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from sort_tools import quicksort
 from webdriver_manager.chrome import ChromeDriverManager
-
-from .sort import quicksort
 
 ATTRIBUTE_ID = {
     "id": By.ID,
